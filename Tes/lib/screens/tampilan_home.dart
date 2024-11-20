275be156b354042e820b24458e606d399fb5873e
MGilangSeftian <143242901+MGilangSeftian@users.noreply.github.com>
2024-11-20 20:45:35 +0700

    baru sampai container atas

--- Tes/lib/screens/tampilan_home.dart
+++ Tes/lib/screens/tampilan_home.dart
@@ -10,6 +10,52 @@ class TampilanHome extends StatefulWidget {
 class _TampilanHomeState extends State<TampilanHome> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('Home',
+        style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold),
+      ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(8),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Telusuri',
+                prefixIcon: const Icon(Icons.search),
+                border:OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: const BorderSide(),
+                ),
+                filled:true,
+                fillColor:Colors.white,
+              )
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/Mobile_login.png'), // Replace with your asset image path
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+
+
+
+        ],
+      ),
+
+    );
   }
 }
